a2cc43d3d23a8fde17d057c32c487f823f65d495
aCrYoZ <acryozps@gmail.com>
2024-12-01 19:00:45 +0300

    final revision

--- src/entities.cpp
+++ src/entities.cpp
@@ -1,24 +1,28 @@
 #include "entities.h"
 #include <ncurses.h>
+#include <array>
 
-void init_entities(chtype* entities) {
-    /*start_color();*/
-    /*init_pair(1, COLOR_YELLOW, COLOR_BLACK);*/
-    /*init_pair(2, COLOR_GREEN, COLOR_BLACK);*/
-    /*init_pair(3, COLOR_WHITE, COLOR_BLACK);*/
-    /*init_pair(4, COLOR_RED, COLOR_BLACK);*/
-    /*init_pair(5, COLOR_MAGENTA, COLOR_BLACK);*/
-    /*entities[EntitiyIndex::Empty] |= COLOR_PAIR(3);*/
-    /*entities[EntitiyIndex::Sunflower] |= COLOR_PAIR(1);*/
-    /*entities[EntitiyIndex::NotReadyPeashooter] |= COLOR_PAIR(2);*/
-    /*entities[EntitiyIndex::ReadyPeashooter] |= COLOR_PAIR(2);*/
-    /*entities[EntitiyIndex::Pea] |= COLOR_PAIR(2);*/
-    /*entities[EntitiyIndex::Wallnut] |= COLOR_PAIR(1);*/
-    /*entities[EntitiyIndex::DamagedWallnut] |= COLOR_PAIR(1);*/
-    /*entities[EntitiyIndex::SmallZombie] |= COLOR_PAIR(4);*/
-    /*entities[EntitiyIndex::MediumZombie] |= COLOR_PAIR(4);*/
-    /*entities[EntitiyIndex::BigZombie] |= COLOR_PAIR(4);*/
-    /*entities[EntitiyIndex::Lawnmower] |= COLOR_PAIR(5);*/
-    /*entities[EntitiyIndex::ReadyLawnmowerBase] |= COLOR_PAIR(5);*/
-    /*entities[EntitiyIndex::NotReadyLawnmowerBase] |= COLOR_PAIR(5);*/
+std::array<const chtype, 13> init_entities() {
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    chtype empty = '.' | COLOR_PAIR(3);
+    chtype sf = '%' | COLOR_PAIR(1);
+    chtype nrp = '?' | COLOR_PAIR(2);
+    chtype rp = '!' | COLOR_PAIR(2);
+    chtype p = '*' | COLOR_PAIR(2);
+    chtype wn = '#' | COLOR_PAIR(1);
+    chtype dwn = '|' | COLOR_PAIR(1);
+    chtype sz = '&' | COLOR_PAIR(4);
+    chtype mz = '$' | COLOR_PAIR(4);
+    chtype bz = '@' | COLOR_PAIR(4);
+    chtype lm = '>' | COLOR_PAIR(5);
+    chtype rlmb = '~' | COLOR_PAIR(5);
+    chtype nrlmb = '=' | COLOR_PAIR(5);
+
+    return std::array<const chtype, 13>{ empty, sf, nrp, rp, p,    wn,   dwn,
+                                         sz,    mz, bz,  lm, rlmb, nrlmb };
 }
